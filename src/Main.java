c7e243d0fe9c53a582d2451a5023ee887d50d918
AlphinShabu <alphin0812@yahoo.com>
2026-03-17 14:04:03 +0530

    UC2:Room Types

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,48 @@
-class UseCase1HotelBookingApp {
+class Room {
+    protected int type;
+    protected int size;
+    protected double price;
 
-    private static final String APP_NAME = "Hotel Booking Management System";
+    public Room(int type, int size, double price) {
+        this.type = type;
+        this.size = size;
+        this.price = price;
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+class UseCase2RoomInitialization {
 
     public static void main(String[] args) {
-        displayWelcomeMessage();
-    }
 
-    private static void displayWelcomeMessage() {
-        System.out.println("Welcome to the " + APP_NAME);
-        System.out.println("System initialized successfully.");
+        SingleRoom single = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        int singleAvailable = 10;
+        int doubleAvailable = 5;
+        int suiteAvailable = 2;
+
+        System.out.println("Room Types and Availability:");
+        System.out.println("Single Room - Available: " + singleAvailable);
+        System.out.println("Double Room - Available: " + doubleAvailable);
+        System.out.println("Suite Room - Available: " + suiteAvailable);
     }
 }
